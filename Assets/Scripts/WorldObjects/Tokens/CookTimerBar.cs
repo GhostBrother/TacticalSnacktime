6503a110a206f8263e02c9770a04312ac7d20fc1
Nathan Choquette <natechoquette@gmail.com>
2019-09-01 00:19:46 -0500

    created my first shader for the food doneness bar

---
+++ Assets/Scripts/WorldObjects/Tokens/CookTimerBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookTimerBar : MonoBehaviour
+{
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+
+    private void Start()
+    {
+        gradient = new Gradient();
+
+        colorKey = new GradientColorKey[2];
+
+        colorKey[0].color = Color.red;
+        colorKey[1].color = Color.black;
+        
+    }
+}
